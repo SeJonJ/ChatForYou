e99d1e17e3d9ef7c6c028633466b144797f746b2
sejon <wkdtpwhs@gmail.com>
2025-08-06 22:29:34 +0900

    #89 SSE 기능 개선 및 Kafka 활용 :: kafka 환경 설정 및 test 코드 생성

---
+++ springboot-backend/src/test/java/webChat/service/kafka/KafkaTest.java
@@ -0,0 +1,35 @@
+package webChat.service.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@Slf4j
+public class KafkaTest {
+    @MockBean
+    private ServletServerContainerFactoryBean webSocketContainer;
+    @Autowired
+    private KafkaProducerTest kafkaProducerTest;
+    @Autowired
+    private KafkaConsumerTest kafkaConsumerTest;
+
+    @Test
+    @DisplayName("kafka test")
+    void sendAndReceive() throws Exception {
+        kafkaConsumerTest.resetLatch();            // Latch 초기화
+        kafkaProducerTest.sendMessage("test-topic", "hello"); // 메시지 전송
+        boolean ok = kafkaConsumerTest.getLatch()
+                .await(10, TimeUnit.SECONDS); // 최대 5초 대기
+        assertTrue(ok, "메시지가 소비되지 않았습니다");
+    }
+
+}
