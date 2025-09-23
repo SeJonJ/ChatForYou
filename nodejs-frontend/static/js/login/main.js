8f20c10518ea52b7d160cf4625655b3a4720fda2
taesik <dlwhsktm@gmail.com>
2025-09-24 00:18:14 +0900

    #67 프론트 밑작업

--- nodejs-frontend/static/js/login/main.js
+++ nodejs-frontend/static/js/login/main.js
@@ -1,3 +1,5 @@
+// const { initializeApp } = require("firebase/app");
+//import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 
 (function ($) {
     "use strict";
@@ -81,6 +83,61 @@
             showPass = 0;
         }
         
+    });
+
+    $('#loginBtn').on('click', function() {
+        alert('해당 기능은 미구현입니다.');
+    });
+
+    $('#googleOauth').on('click', function(e) {
+        var projectId = window.__CONFIG__.GOOGLE_OAUTH.PROJECT_ID;
+        var apiKey = window.__CONFIG__.GOOGLE_OAUTH.API_KEY;
+        var authDomain = window.__CONFIG__.GOOGLE_OAUTH.AUTH_DOMAIN;
+
+        var firebaseapp = firebase.initializeApp({
+            projectId: projectId,
+            apiKey: apiKey,
+            authDomain: authDomain
+        });
+
+        var auth = getAuth(firebaseapp);
+
+        var googleAuthProvider = new GoogleAuthProvider();
+        googleAuthProvider.setDefaultLanguage('ko');
+        googleAuthProvider.setCustomParameters({
+            login_hint: 'user@example.com'
+        });
+
+        signInWithPopup(auth, googleAuthProvider)
+        .then((userCredential) => {
+            userCredential.user.getIdToken()
+            .then((idToken) => {
+                console.log('idToken : ' + idToken);
+                var refreshToken = userCredential.user.refreshToken;
+                console.log('refresh Token : ' + refreshToken);
+            });
+        });
+
+        // var url = "https://accounts.google.com/o/oauth2/v2/auth";
+        // var width = 500;
+        // var height = 600;
+        // var left = (window.screen.width / 2) - (width / 2);
+        // var top = (window.screen.height / 2) - (height / 2);
+
+        // var clientId = window.__CONFIG__.GOOGLE_OAUTH.CLIENT_ID;
+        // var redirectURL = window.__CONFIG__.GOOGLE_OAUTH.REDIRECT_URL;
+        // var responseType = window.__CONFIG__.GOOGLE_OAUTH.RESPONSE_TYPE;
+        // var scope = window.__CONFIG__.GOOGLE_OAUTH.SCOPE;
+
+        // url += '?client_id=' + clientId + '&redirect_uri=' + redirectURL
+        //  + '&response_type=' + responseType + '&scope=' + scope;
+
+
+        // window.open(
+        //     url,
+        //     "Google",
+        //     `width=${width},height=${height},left=${left},top=${top},resizable=no,scrollbars=no`
+        // );
     });
 
 
--- springboot-backend/src/main/java/webChat/controller/LoginController.java
+++ springboot-backend/src/main/java/webChat/controller/LoginController.java
@@ -1,5 +1,6 @@
 package webChat.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,5 +15,11 @@ public class LoginController {
         // docker 로 실행 시 thymeleaf 에러 발생 => 경로 문제로 인한 에러인듯
         // 따라서 기존의 /chatlogin -> chatlogin 으로 변경
         return "chatlogin";
+    }
+
+    @GetMapping("/googleOauth")
+    public void googleOauth(HttpServletRequest request) {
+        String requestURL = request.getRequestURL().toString(); // 기본 URL
+        String queryString = request.getQueryString();
     }
 }
