e0b64dcf45998d450235f237f5926ace495446b0
sejon <wkdtpwhs@gmail.com>
2026-02-14 17:13:57 +0900

    #106 녹화 파일 다운로드 기록 기능 추가 :: 일반 파일 및 녹화 파일 다운로드 시 로그 저장 기능 추가

--- springboot-backend/src/main/java/webChat/ChatApplication.java
+++ springboot-backend/src/main/java/webChat/ChatApplication.java
@@ -1,8 +1,12 @@
 package webChat;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Date;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +14,17 @@ public class ChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
+	}
+
+	@PostConstruct
+	public void started() {
+		// 애플리케이션 전역 타임존을 Asia/Seoul로 고정
+		System.out.println("###########################################################################");
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+		System.out.println("############### ChatForYou TimeZone :: " + TimeZone.getDefault().getID() + "###############");
+		System.out.println("############### ChatForYou StartAt :: " + new Date() + "###############");
+		System.out.println("###########################################################################");
+
 	}
 
 }
